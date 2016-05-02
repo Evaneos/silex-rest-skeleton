fcf4d30c1a1f349ff2868a532b8b213e6c7646a4
remi-san <remi.san@gmail.com>
2016-05-02 14:34:46 +0200

    harmonized debug management between http and cli

--- public/index.php
+++ public/index.php
@@ -6,7 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__ . '/../vendor/autoload.php';
 
 $env = getenv('SILEX_SKT_ENV') ?: 'dev';
-$kernel = new HttpKernel($env, $env !== 'prod', \Ramsey\Uuid\Uuid::uuid4());
+$debug = getenv('SILEX_SKT_DEBUG') !== '0' && $env !== 'prod';
+$kernel = new HttpKernel($env, $debug, \Ramsey\Uuid\Uuid::uuid4());
 $response = $kernel->handle($request = Request::createFromGlobals());
 $response->send();
 $kernel->terminate($request, $response);
