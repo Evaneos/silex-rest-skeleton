097a0fc50c78aca6b15275e0569f55ea22fcb168
remi-san <remi.san@gmail.com>
2016-05-04 16:45:12 +0200

    fixed uuid use

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 
 use Evaneos\REST\HttpKernel;
 use Symfony\Component\HttpFoundation\Request;
+use Ramsey\Uuid\Uuid;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $env = getenv('SILEX_SKT_ENV') ?: 'dev';
 $debug = getenv('SILEX_SKT_DEBUG') !== '0' && $env !== 'prod';
-$kernel = new HttpKernel($env, $debug, \Ramsey\Uuid\Uuid::uuid4());
+$kernel = new HttpKernel($env, $debug, (string) Uuid::uuid4());
 $response = $kernel->handle($request = Request::createFromGlobals());
 $response->send();
 $kernel->terminate($request, $response);
