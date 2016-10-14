708feac37a32072e37ca01f388030c28a524a1b5
Alexandre Dupuy <alex.dpy@gmail.com>
2016-10-14 18:00:36 +0200

    forward x-request-id header

--- public/index.php
+++ public/index.php
@@ -2,13 +2,12 @@
 
 use Evaneos\REST\HttpKernel;
 use Symfony\Component\HttpFoundation\Request;
-use Ramsey\Uuid\Uuid;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $env = getenv('SILEX_SKT_ENV') ?: 'dev';
 $debug = getenv('SILEX_SKT_DEBUG') !== '0' && $env !== 'prod';
-$kernel = new HttpKernel($env, $debug, (string) Uuid::uuid4());
+$kernel = new HttpKernel($env, $debug);
 $response = $kernel->handle($request = Request::createFromGlobals());
 $response->send();
 $kernel->terminate($request, $response);
